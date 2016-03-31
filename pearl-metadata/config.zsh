1c1de8e26ee265b3caa0ddb5b214d45eaeb1dc70
Filippo Squillace <squillace.filippo@gmail.com>
2016-03-31 21:48:58 +0100

    First commit!

---
+++ pearl-metadata/config.zsh
@@ -0,0 +1,4 @@
+# Path to oh-my-zsh installation.
+export ZSH=$PEARL_HOME/packages/oh-my-zsh/module
+
+source $ZSH/oh-my-zsh.sh
---
+++ pearl-metadata/install.sh
@@ -0,0 +1,20 @@
+
+function _config_information(){
+    info "The oh-my-zsh config template is located in:"
+    echo "    $PEARL_HOME/packages/oh-my-zsh/module/templates/zshrc.zsh-template"
+    info "Put all the oh-my-zsh config you want in ~/.zshrc BEFORE the pearl source command that should look like:"
+    echo "    source $PEARL_ROOT/boot/pearl.sh"
+    return 0
+}
+
+function post_install(){
+    _config_information
+}
+
+function post_update(){
+    _config_information
+}
+
+function pre_remove(){
+    return 0
+}
